85af21104c5046d621a79bba339cdd784bea62f5
Moeez Agha <aghamoeez@gmail.com>
2025-09-02 16:50:58 -0400

    Added Radzen UI

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using OllamaLocalConnect.Data;
+using Radzen;
 
 namespace OllamaLocalConnect
 {
@@ -14,6 +15,7 @@ namespace OllamaLocalConnect
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddSingleton<WeatherForecastService>();
+            builder.Services.AddRadzenComponents();
 
             var app = builder.Build();
 
